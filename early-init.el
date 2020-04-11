3adaf5bdfa12df80be2cdf1aeb633a5895c547d7
Ryan Hajianpour <ryan.hajianpour@gmail.com>
2020-04-11 20:22:14 +0430

    Add GC optimization for faster startup

---
+++ early-init.el
@@ -0,0 +1,30 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(push '(menu-bar-lines . 1) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "Ubuntu Mono"
+                    :height 130
+                    :weight 'normal
+                    :width 'normal)
+
+;; Ignore X resources; its settings would be redundant with the other settings
+;; in this file and can conflict with later config (particularly where the
+;; cursor color is concerned).
+(advice-add #'x-apply-session-resources :override #'ignore)
+
--- init.el
+++ init.el
@@ -2,13 +2,34 @@
 ;;; Commentary:
 ;;; Source located at https://github.com/Borderliner/RME
 
-;;; Code:
+;;; GC and other startup optimizations
+
+;; A big contributor to startup times is garbage collection.  We up the GC
+;; threshold to temporarily prevent it from running, and then reset it later
+;; using a hook.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; Keep a ref to the actual file-name-handler
+(defvar default-file-name-handler-alist file-name-handler-alist)
+
+;; Set the file-name-handler to nil (because regexing is cpu intensive)
+(setq file-name-handler-alist nil)
+
+;; Reset file-name-handler-alist after initialization
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 16777216
+          gc-cons-percentage 0.1
+          file-name-handler-alist default-file-name-handler-alist)))
+
+;;; Add conf folder to the load path
 (add-to-list 'load-path (expand-file-name "conf" user-emacs-directory))
 
 ;;; Core modules
 (require 'init-core)
 (require 'init-ui)
-(require 'init-editting)
+(require 'init-editing)
 
 ;;; Helper modules
 (require 'init-company)
