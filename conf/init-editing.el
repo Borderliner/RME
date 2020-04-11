d69cfd618fa0f2bb6f4526b1bd987c80324fdd4a
Ryan Hajianpour <ryan.hajianpour@gmail.com>
2020-04-11 20:56:04 +0430

    Add a fix for emacs killing text when using M-backspace or C-backspace

--- conf/init-editing.el
+++ conf/init-editing.el
@@ -23,10 +23,26 @@
 ;; Remember line when you revisit a file
 (setq-default save-place t)
 (setq save-place-file (concat user-emacs-directory "places"))
-
 (setq delete-active-region t)
+
+;; Fix for emacs killing text to kill-ring when using M-Backspace and C-Backspace
+(defun delete-word (arg)
+  "Delete characters forward until encountering the end of a word.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (delete-region (point) (progn (forward-word arg) (point))))
+
+(defun backward-delete-word (arg)
+  "Delete characters backward until encountering the beginning of a word.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (delete-word (- arg)))
+
+(global-set-key (kbd "<M-backspace>") 'backward-delete-word)
+(global-set-key (kbd "<C-backspace>") 'backward-delete-word)
 
 ;; Disable autosaves
 ; (setq auto-save-default nil)
 
 (provide 'init-editing)
+
